aadc807e5c06df72b6915617fdc5ab7aee9ffaec
AndyMore <morteyandrews@gmail.com>
2024-07-21 02:46:11 +0100

    Update API

--- api/api.py
+++ api/api.py
@@ -7,33 +7,50 @@ import pandas as pd
 app = FastAPI()
 
 class DataFeatures(BaseModel):
-    Plasma_Glucose: int
-    Blood_Work_Result-1: int
-    Blood_Pressure: int
-    Blood_Work_Result-2: int
-    Blood_Work_Result-3: int
-    Body_Mass_Index: float
-    Blood_Work_Result-4: float
-    Age: int
-    Insurance: int
+    plasma_glucose: int
+    blood_work_result_1: int
+    blood_pressure: int
+    blood_work_result_2: int
+    blood_work_result_3: int
+    body_mass_index: float
+    blood_work_result_4: float
+    age: int
+    insurance: int
 
 
 @app.get('/')
 def status_check():
     return {"Status": "API is Online..."}
 
-
+#Load models and encoder
 logistic_regression = joblib.load('../Models/logistic_regression_pipeline.pkl')
+random_forest = joblib.load('../Models/random_forest_pipeline.pkl')
 encoder = joblib.load('../Models/label_encoder.pkl')
 
-@app.post('/logistic_prediction')
+@app.post('/predict')
 def predict_sepsis(data: DataFeatures):
-
+     
+    #Convert input data to DataFrame
     df = pd.DataFrame([data.model_dump()])
 
-    prediction = logistic_regression.predict(df)
+    #Make predictions
+    prediction_log = logistic_regression.predict(df)
+    prediction_ran = random_forest.predict(df)
+    
+    #Convert predictions to int
+    prediction_log = int(prediction_log)
+    prediction_ran = int(prediction_ran)
+    
+    #Inverse transform predictions
+    prediction_log = encoder.inverse_transform([prediction_log])[0]
+    prediction_ran = encoder.inverse_transform([prediction_ran])[0]
 
-    prediction = int(prediction)
+    return {"logistic_regression_prediction": prediction_log,
+            "random_forest_prediction": prediction_ran
+            }
 
-    return {"prediction": prediction}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
